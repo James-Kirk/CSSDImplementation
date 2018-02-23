8278449ac62560c7e02e4bf6d411cbaa5c76d60b
Alex <alexogden@outlook.com>
2018-02-23 19:11:25 +0000

    Removed passenger

--- CSSD_Transport/CSSD_Transport/Accounts/Account.cs
+++ CSSD_Transport/CSSD_Transport/Accounts/Account.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace CSSD_Transport.Accounts
 {
     [Serializable]
-	public abstract class Account : IPassenger
+	public abstract class Account
 	{
 		private string username, password, type;
 		private float creditAmount;
