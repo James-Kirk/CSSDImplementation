5fcd4a7872ecb02075ecdd4ba988b139d682bda1
Alex <alexogden@outlook.com>
2018-03-07 16:26:04 +0000

    SetOfTokens now singleton

--- CSSD_Transport/CSSD_Transport/Tokens/SetOfTokens.cs
+++ CSSD_Transport/CSSD_Transport/Tokens/SetOfTokens.cs
@@ -4,11 +4,23 @@ using System.Threading.Tasks;
 
 namespace CSSD_Transport.Tokens
 {
-	public class SetOfTokens : List<Token>
+	public class SetOfTokens
 	{
+		private List<Token> tokens = new List<Token>();
+		private static SetOfTokens instance;
+
+		public SetOfTokens() { }
+
+		public static SetOfTokens Instance
+		{
+			get { return instance == null ? instance = new SetOfTokens() : instance; }
+
+			set { instance = value; }
+		}
+
 		public Token findToken(int id)
 		{
-			return this.Find(i => i.getID() == id);
+			return tokens.Find(i => i.getID() == id);
 		}
 	}
 }
