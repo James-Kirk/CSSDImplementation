bbf87129ac9e6700bda1617c47cf3f4e5ed66c93
Kirk <b4012447@hallam.shu.ac.uk>
2018-03-15 12:56:57 +0000

    adding in init tests to tokentests

--- CSSD_Transport/CSSD_TransportTests/Token/TokenTests.cs
+++ CSSD_Transport/CSSD_TransportTests/Token/TokenTests.cs
@@ -5,12 +5,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSSD_Transport.Tokens;
+using CSSD_Transport.Accounts;
 
 namespace CSSD_Transport.Token.Tests
 {
     [TestClass()]
     public class TokenTests
     {
+        private Ticket tk;
+        private SmartCard sc;
+        private NormalAccount ac1;
+        private NormalAccount ac2;
+
+        [TestInitialize()]
+        public void tokenSetup()
+        {
+            ac1 = new NormalAccount("Ben", "Watt", DateTime.Now);
+            ac2 = new NormalAccount("Ellie", "Full", DateTime.Now);
+            tk = new Ticket(new Journeys.Location("Kings Cross"), new Journeys.Location("Victoria"), ac1, DateTime.Now);
+            sc = new SmartCard(ac2, false, 0);
+        }
+
         [TestMethod()]
         public void getScannedStatusTest()
         {
